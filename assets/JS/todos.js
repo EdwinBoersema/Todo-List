8248395d2d9a4f296d91bcecd2a94c2007faf02a
Edwin Boersema <edwinboersema@gmail.com>
2019-10-15 11:20:17 +0200

    styling part 2

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -18,6 +18,6 @@ $("input[type='text']").keypress(function(event){
         let todoText = $(this).val();
         $(this).val("");
         // creating a new li and adding it to the lu
-        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span>" + todoText + "</li>");
     }
 });
