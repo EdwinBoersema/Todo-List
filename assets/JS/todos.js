b77f89ef829fddca3b3a8f47e5f521f6397a431c
Edwin Boersema <edwinboersema@gmail.com>
2019-10-15 09:32:32 +0200

    added  completion toggle and deletion of todo's

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,2 +1,13 @@
-// check off todos
-$("li").click
+// toggle todos
+$("li").click(function(){
+    $(this).toggleClass("completed");
+});
+
+// click on X to delete Todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
