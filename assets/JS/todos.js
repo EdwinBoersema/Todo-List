af26f56be3fb95d2539bee0022f50d1b36e59806
Edwin Boersema <edwinboersema@gmail.com>
2019-10-15 09:48:25 +0200

    Completed the js functionality of the Todo List

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,13 +1,23 @@
 // toggle todos
-$("li").click(function(){
+$("ul").on('click', "li", function(){
     $(this).toggleClass("completed");
 });
 
 // click on X to delete Todo
-$("span").click(function(event){
+$("ul").on('click', "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
 });
 
+// Creating new todo on input
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // getting the value of the input
+        let todoText = $(this).val();
+        $(this).val("");
+        // creating a new li and adding it to the lu
+        $("ul").append("<li><span>X </span>" + todoText + "</li>");
+    }
+});
